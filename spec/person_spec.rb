58072208c664db3574c07ec4a1bc6f15185a9232
Blake Dempsey <bdempsey864@gmail.com>
2021-06-03 09:28:49 -0600

    Iteration 1 complete

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,5 +7,22 @@ RSpec.describe Person do
 
     expect(kathy).to be_a(Person)
   end
+
+  it 'has readable attributes' do
+    kathy = Person.new('Kathy Chan', 10)
+
+    expect(kathy.name).to eq('Kathy Chan')
+    expect(kathy.experience).to eq(10)
+    expect(kathy.specialties).to eq([])
+  end
+
+  it 'adds specialties' do
+    kathy = Person.new('Kathy Chan', 10)
+
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+
+    expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+  end
 end
 
